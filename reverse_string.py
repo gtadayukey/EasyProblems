e8b75f3f3c219bd4900965dd8d42a64a39809323
gtadayukey <gtadayukey@gmail.com>
2024-04-15 15:26:14 -0300

    implement reverse string testcase

--- reverse_string.py
+++ reverse_string.py
@@ -1,4 +1,4 @@
-def reverseString(self, s):
+def reverseString(s):
     i = 0
 
     while len(s) / 2 > i:
@@ -6,3 +6,11 @@ def reverseString(self, s):
         s[i] = s[(i + 1) * -1]
         s[(i + 1) * -1] = temp
         i += 1
+
+    return s
+
+
+if __name__ == "__main__":
+    s1 = ['H', 'e', 'l', 'l', 'o', ' ', 'e', 'v', 'e', 'r', 'y', 'o', 'n', 'e', '!']
+    reverseString(s1)
+    print(s1)
